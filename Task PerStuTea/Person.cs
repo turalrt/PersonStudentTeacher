acabb785f9ff55bc7f6f592c7bd2db264abdd25a
Tural Taghiyev <turalrt@code.edu.az>
2021-12-05 23:33:52 +0400

    first commit

---
+++ Task PerStuTea/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_PerStuTea
+{
+    internal class Person
+    {
+        public void Greet()
+        {
+
+        }
+        public void SetAge(int a)
+        {
+
+        }
+    }
+}
---
+++ Task PerStuTea/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_PerStuTea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Greet();
+
+            Teacher teacher = new Teacher();
+            teacher.Greet();
+            teacher.SetAge(43);
+            teacher.Explain();
+
+
+            Student student = new Student();
+            student.Greet();
+            student.SetAge(17);
+            student.ShowAge();
+            student.Study();
+        }
+    }
+}
